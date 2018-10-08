b2f099177f63fa351bbf391b4fe7feb47563e235
Britney Smith <brit.smith7@gmail.com>
2018-10-08 10:50:25 -0400

    initial commit of xcode project

---
+++ SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFWCardsAgainstHumanity
+//
+//  Created by Britney Smith on 10/8/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
