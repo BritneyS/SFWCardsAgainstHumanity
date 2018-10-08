928dead20f12b3e393b38044f37db6ea6208a1b4
Britney Smith <brit.smith7@gmail.com>
2018-10-08 15:59:37 -0400

    Connected blackCardLabel and whiteCardPhrase button outlets

--- SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Controllers/PlayViewController.swift
+++ SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Controllers/PlayViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class PlayViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var blackCardLabel: UILabel!
+    @IBOutlet weak var whiteCardPhrase1Button: UIButton!
+    @IBOutlet weak var whiteCardPhrase2Button: UIButton!
+    @IBOutlet weak var whiteCardPhrase3Button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
