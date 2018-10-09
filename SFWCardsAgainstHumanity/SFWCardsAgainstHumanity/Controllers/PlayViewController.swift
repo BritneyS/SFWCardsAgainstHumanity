df6108942c2fdbf87bc55a0ecb0cc7542e7f7bb3
Britney Smith <brit.smith7@gmail.com>
2018-10-09 10:33:56 -0400

    Added populateDeck method

--- SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Controllers/PlayViewController.swift
+++ SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Controllers/PlayViewController.swift
@@ -17,10 +17,42 @@ class PlayViewController: UIViewController {
     @IBOutlet weak var whiteCardPhrase2Button: UIButton!
     @IBOutlet weak var whiteCardPhrase3Button: UIButton!
     
+    // MARK: Properties
+    var whiteCards: [String] = []
+    var blackCards: [BlackCard] = []
+    
+    var blackCard: BlackCard?
+    var whiteCard1: String?
+    var whiteCard2: String?
+    var whiteCard3: String?
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        populateDeck()
+        print("test")
+        
+    }
+    
+    func populateDeck() {
+        print("loading all cards")
+        let deckDatabase = DeckDatabase()
+        
+        for whiteCard in deckDatabase.whiteCards {
+            whiteCards.append(whiteCard)
+        }
+        
+        for blackCard in deckDatabase.blackCards {
+            blackCards.append(blackCard)
+        }
+        
+        for card in blackCards {
+            print("BlackCards: \(card.text!) + \(card.pick!)")
+        }
+        print("White cards: \(whiteCards)")
+        
+        
     }
     
 
--- SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Model/DeckDatabase.swift
+++ SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Model/DeckDatabase.swift
@@ -10,18 +10,22 @@ import Foundation
 
 struct DeckDatabase {
     let blackCards = [
-        [
-            "What's that smell?",
-            1
-        ],
-        [
-            "_ is a slippery slope that leads to _.",
-            2
-        ],
-        [
-            "I set up script to buy things on ebay for $1, but then it bought _, _, and _.",
-            3
-        ]
+//        [
+//            "What's that smell?",
+//            1
+//        ],
+//        [
+//            "_ is a slippery slope that leads to _.",
+//            2
+//        ],
+//        [
+//            "I set up script to buy things on ebay for $1, but then it bought _, _, and _.",
+//            3
+//        ]
+        
+        BlackCard(text: "What's that smell?", pick: 1),
+        BlackCard(text: "_ is a slippery slope that leads to _.", pick: 2),
+        BlackCard(text: "I set up script to buy things on ebay for $1, but then it bought _, _, and _.", pick: 3)
     ]
     
     let whiteCards = [
