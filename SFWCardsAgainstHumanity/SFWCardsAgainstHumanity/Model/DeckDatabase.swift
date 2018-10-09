5e949af379d79f3b08cd62ee762b36b78d5981c3
Britney Smith <brit.smith7@gmail.com>
2018-10-09 09:34:19 -0400

    Added model for JSON data

---
+++ SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Model/DeckDatabase.swift
@@ -0,0 +1,9 @@
+//
+//  DeckDatabase.swift
+//  SFWCardsAgainstHumanity
+//
+//  Created by Britney Smith on 10/9/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
