7e56cd9179d734df2d6e6aea1b68bf8eca2be8f5
Britney Smith <brit.smith7@gmail.com>
2018-10-09 09:46:15 -0400

    Created temporary database for mock data

--- SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Model/DeckDatabase.swift
+++ SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Model/DeckDatabase.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+struct DeckDatabase {
+    let blackCards = [
+        [
+            "What's that smell?",
+            1
+        ],
+        [
+            "_ is a slippery slope that leads to _.",
+            2
+        ],
+        [
+            "I set up script to buy things on ebay for $1, but then it bought _, _, and _.",
+            3
+        ]
+    ]
+    
+    let whiteCards = [
+        "Sharing needles.",
+        "Friction.",
+        "A mime having a stroke.",
+        "A hot mess.",
+        "Adderall&trade;.",
+        "A windmill full of corpses."
+    ]
+}
