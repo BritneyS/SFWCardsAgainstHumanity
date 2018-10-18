b3587eabda3917f6dde55f05bcb334712b5b729e
Britney Smith <brit.smith7@gmail.com>
2018-10-18 16:21:24 -0400

    Added FavoritesViewController, set up segue from 'Faves' button to FavoritesViewController, set segue and storyboard IDs

---
+++ SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Controllers/FavoritesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesViewController.swift
+//  SFWCardsAgainstHumanity
+//
+//  Created by Britney Smith on 10/18/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
