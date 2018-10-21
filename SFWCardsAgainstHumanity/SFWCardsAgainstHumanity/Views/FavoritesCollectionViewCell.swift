9d79c9c87701da162e92fcb543a21715b4d9598f
Britney Smith <brit.smith7@gmail.com>
2018-10-21 18:39:54 -0400

    Added custom cell, reuse identifier, and label

---
+++ SFWCardsAgainstHumanity/SFWCardsAgainstHumanity/Views/FavoritesCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesCollectionViewCell.swift
+//  SFWCardsAgainstHumanity
+//
+//  Created by Britney Smith on 10/21/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var blackCardLabel: UILabel!
+    
+    
+}
